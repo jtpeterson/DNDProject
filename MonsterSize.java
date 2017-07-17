5db4b7705c02c91f8cac24869a19ffce89443bc3
jtpeterson <jpeterson@gatech.edu>
2017-07-17 13:09:22 -0400

    Added all important stats except reach to the templates

--- MonsterSize.java
+++ MonsterSize.java
@@ -36,4 +36,85 @@ public enum MonsterSize {
             return ("hacker");
         }
     }
+
+    public int getAttackACModifier() {
+    	switch (size) {
+        case 1:
+            return (16);
+        case 2:
+            return (12);
+        case 3:
+            return (10);
+        case 4:
+            return (9);
+        case 5:
+        	return (8);
+        case 6:
+        	return (7);
+        case 7:
+        	return (6);
+        case 8:
+        	return (4);
+        case 9:
+        	return (0);
+        case 10:
+        	return (0);
+        default:
+            return ("hacker");
+    	}
+	}
+
+    public int getSpecialAttackModifier() {
+    	switch (size) {
+        case 1:
+            return (0);
+        case 2:
+            return (4);
+        case 3:
+            return (8);
+        case 4:
+            return (12);
+        case 5:
+        	return (16);
+        case 6:
+        	return (20);
+        case 7:
+        	return (24);
+        case 8:
+        	return (28);
+        case 9:
+        	return (32);
+        case 10:
+        	return (32);
+        default:
+            return ("hacker");
+    	}
+    }
+
+    public int getHideModifier() {
+    	switch (size) {
+        case 1:
+            return (32);
+        case 2:
+            return (28);
+        case 3:
+            return (24);
+        case 4:
+            return (20);
+        case 5:
+        	return (16);
+        case 6:
+        	return (12);
+        case 7:
+        	return (8);
+        case 8:
+        	return (4);
+        case 9:
+        	return (0);
+        case 10:
+        	return (0);
+        default:
+            return ("hacker");
+    	}
+    }
 }
